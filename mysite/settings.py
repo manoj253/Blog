c31ffa190332a617bf2446ee613e1c9cdb054b5c
manoj253 <cse.manoj253@gmail.com>
2016-02-23 23:59:21 +0530

    Sharing posts by e-mail

--- mysite/settings.py
+++ mysite/settings.py
@@ -120,3 +120,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/1.9/howto/static-files/
 
 STATIC_URL = '/static/'
+
+
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_HOST_USER = 'manoj111253cse@gmail.com'
+EMAIL_HOST_PASSWORD = '7607789271'
+EMAIL_PORT = 587
+EMAIL_USE_TLS = True
